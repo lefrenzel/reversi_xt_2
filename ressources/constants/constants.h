bedb73af97c995d24cdb0825f58d5eb840328ffe
Lennart Frenzel <lenni.frenzel@gmail.com>
2024-08-30 16:34:39 +0200

    split constant reading from main

---
+++ ressources/constants/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <iostream>
+
+extern std::string HELP_TEXT;
+extern std::string STANDARD_IP;
+extern std::string STANDARD_PORT;
+extern std::string STANDARD_DEPTH;
+extern std::string STANDARD_TIME;
+
+void parseConfigFile();
+
+#endif //CONSTANTS_H
