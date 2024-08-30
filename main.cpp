6ac2153d05e23585ea4ad43beaaa72fdfa092d2e
Lennart Frenzel <lenni.frenzel@gmail.com>
2024-08-30 16:36:17 +0200

    removed config file reader from main

--- main.cpp
+++ main.cpp
@@ -3,11 +3,8 @@
 #include <getopt.h>
 #include <iostream>
 #include <fstream>
-#include <map>
+#include "./ressources/constants/constants.h"
 
-// Global variables
-std::string CONFIG_PATH = "./ressources/config/client_globals.rc";
-std::map<std::string, std::string> CONFIG;
 
 class Main {
     public:
@@ -15,7 +12,7 @@ class Main {
             for (int i = 0; i < argc; i++) {
                 std::string args(argv[i]);
                 if (args == "-h" || args == "--help") {
-                    
+                    std::cout << HELP_TEXT << std::endl;
                 }
             }
         }
@@ -28,56 +25,6 @@ class Main {
         double timeLimit;
 
 };
-
-/// @brief Parses the config file and stores the read variables in a map
-void parseConfigFile() {
-    std::ifstream configFile(CONFIG_PATH);
-    if(!configFile.is_open()) {
-        std::string errorMessage = "Unable to open config file at " + CONFIG_PATH + ".";
-        throw errorMessage;
-        return;
-    }
-
-    std::string line;
-    std::string currentKey;
-    std::string currentValue;
-
-    while(std::getline(configFile, line)) {
-    
-        line.erase(0, line.find_first_not_of(" \t"));
-        line.erase(line.find_last_not_of(" \t") + 1);
-
-        // skip commented lines
-        if(line.size() > 0 && line[0] == '#'){
-            continue;
-        }
-
-        //skip lines without '='
-        size_t equalsPos = line.find('=');
-        if(equalsPos == std::string::npos) {
-            continue;
-        }
-
-        currentKey = line.substr(0, equalsPos);
-        currentValue = line.substr(equalsPos + 1);
-
-        // Replaces all Escape characters in the config file with reals escape characters
-        equalsPos = 0;
-        while ((equalsPos = currentValue.find("\\n", equalsPos)) != std::string::npos) {
-            currentValue.replace(equalsPos, 2, "\n");
-            equalsPos++;
-        }
-        equalsPos = 0;
-        while ((equalsPos = currentValue.find("\\t", equalsPos)) != std::string::npos) {
-            currentValue.replace(equalsPos, 2, "\t");
-            equalsPos++;    
-        }
-
-        CONFIG[currentKey] = currentValue;
-    }
-    configFile.close();
-    return;
-}
 
 int main(int argc, char* argv[]) {
     try {
