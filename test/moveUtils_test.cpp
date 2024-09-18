d04fea29e699b74275544e06ada0b68f5bc3849e
Lennart Frenzel <lenni.frenzel@gmail.com>
2024-09-18 20:09:47 +0200

    completed test for moveUtils

--- test/moveUtils_test.cpp
+++ test/moveUtils_test.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "moveUtils.h"
+#include <iostream>
 
 uint32_t move_in = 0;
 int coordinateX_in = 0;
@@ -9,13 +9,51 @@ specialInfo specialInfo_in = normal;
 
 int testPlayerNumber() {
   uint32_t move_out = 0;
-  int playerNumber_test = 0;
-  std::cout << "Testing player number handling...";
+  int playerNumber_correct = 0;
   for (playerNumber_in; playerNumber_in <= 8; playerNumber_in++) {
     move_out = setPlayerNumber(move_in, playerNumber_in);
-    playerNumber_test = playerNumber_in & 0xFF;
-    std::cout << std::hex << move_out << "\n";
-    if (playerNumber_test = !getPlayerNumber(move_out)) {
+    playerNumber_correct = playerNumber_in & 0xFF;
+    if (playerNumber_correct != getPlayerNumber(move_out)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int testCoordinateX() {
+  uint32_t move_out = 0;
+  int coordinateX_correct = 0;
+  for (coordinateX_in; coordinateX_in < 50; coordinateX_in++) {
+    move_out = setCoordinateX(move_in, coordinateX_in);
+    coordinateX_correct = move_in & 0xFF000000;
+    if (coordinateX_correct != getCoordinateX(move_out)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int testCoordinateY() {
+  uint32_t move_out = 0;
+  int coordinateY_correct = 0;
+  for (coordinateY_in; coordinateY_in < 50; coordinateY_in++) {
+    move_out = setCoordinateX(move_in, coordinateY_in);
+    coordinateY_correct = move_in & 0x00FF0000;
+    if (coordinateY_correct != getCoordinateY(move_out)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int testSpecialInfo() {
+  uint32_t move_out = 0;
+  for (auto it = specialInfoLookup.begin(); it != specialInfoLookup.end();
+       ++it) {
+    int enumValue = it->first;
+    specialInfo info = it->second;
+    move_out = setSpecialInfo(move_in, info);
+    if (info != getSpecialInfo(move_out)) {
       return 1;
     }
   }
@@ -23,12 +61,13 @@ int testPlayerNumber() {
 }
 
 int main(int argc, char *argv[]) {
-  int coordinateX_out = 0;
   int coordinateY_out = 0;
   int playerNumber_out = 0;
   specialInfo specialInfo_out = normal;
   int testResults[8] = {0};
   testResults[0] = testPlayerNumber();
+  testResults[1] = testCoordinateX();
+  testResults[2] = testCoordinateY();
 
   int arrayElements = sizeof(testResults) / sizeof(testResults[0]);
   for (int i = 0; i < arrayElements; i++) {
